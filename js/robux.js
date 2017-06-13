ad9545f7cb0d35ae0e878d9a33846a921d3cb292
mwear3robloxandroid <mitchellwears@gmail.com>
2017-06-13 11:20:59 -0400

    Create robux.js

---
+++ js/robux.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Add Robux on Roblox 1.2
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @description  Robux Hack
+// @author       You
+// @match        https://www.roblox.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+function java() {
+  
+}
+
+java.prototype = {script
+  
+};
+
+    //robux: (1000000)function add robux()
+    //roblox: (https:roblox.com/home)
+    //add: (//1,000,000)
+    //appear: (/:1,000,000/)
+    function robuxadd1000000() {
+  
+}
+
+robuxadd1000000.prototype = {
+  
+};
+
+})();
